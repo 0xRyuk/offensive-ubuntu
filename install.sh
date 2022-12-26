ec5c14cb421ba63b7e1201de6f7bca67f0beaf1a
Ryuk <ryuk.infosec@gmail.com>
2022-12-26 11:50:50 +0530

    Updated tool versions
    Determine the architecture type of the system
    Add the latest go binary
    Remove unnecessary environment variables
    Add the latest version of maltego

--- install.sh
+++ install.sh
@@ -5,6 +5,20 @@ green='\033[1;32m'
 red='\033[1;31m'
 reset='\033[0m'
 
+# Get the architecture of the system
+architecture=$(uname -m)
+
+# Check the architecture and print a message
+if [ "$architecture" == "x86_64" ]; then
+    AARCH="amd64"
+    elif [ "$architecture" == "i686" || "$architecture" == "i386" ]; then
+    AARCH="386"
+    elif [ "$architecture" == "arm64" ]; then
+    AARCH="arm64"
+else
+    echo "Unable to determine system architecture."
+fi
+
 sudo apt-get update && apt-get upgrade -y
 sleep 2
 sudo apt install figlet
@@ -24,13 +38,13 @@ PACKAGES=("recordmydesktop" "python3" "python2" "hashcat" "vim" "net-tools" "air
 for PKG in ${PACKAGES[@]}
 
 do
-	IS_INSTALLED=$(sudo dpkg-query -W --showformat='${Status}\n' ${PKG} | grep "install ok installed")
-	if [ "${IS_INSTALLED}" == "install ok installed" ]
-	then
-		echo -e "${PKG} is installed."
-	else
-		sudo apt install -y ${PKG}
-	fi
+    IS_INSTALLED=$(sudo dpkg-query -W --showformat='${Status}\n' ${PKG} | grep "install ok installed")
+    if [ "${IS_INSTALLED}" == "install ok installed" ]
+    then
+        echo -e "${PKG} is installed."
+    else
+        sudo apt install -y ${PKG}
+    fi
 done
 
 echo "Installing sublime text3"
@@ -78,21 +92,18 @@ echo "Done"
 
 if [ ! -d /usr/local/go ]
 then
-	echo "Installing golang!!"
-	#sudo snap install go --classic
-	wget https://dl.google.com/go/go1.15.7.linux-amd64.tar.gz
-	sudo tar -C /usr/local/ -xzf go1.15.7.linux-amd64.tar.gz
-	export GOROOT=/usr/local/go
-	export GOPATH=$HOME/go-workspace
-	export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-	echo "export GOROOT=/usr/local/go">>~/.profile
-	echo "export GOPATH=$HOME/go-workspace">>~/.profile
-	echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH">>~/.profile
-	source ~/.profile
-	echo "Done"
-	sleep 2
+    echo "Installing golang!!"
+    #sudo snap install go --classic
+    wget https://dl.google.com/go/go1.19.4.linux-${AARCH}.tar.gz
+    sudo tar -C /usr/local/ -xzf go1.19.4.linux-${AARCH}.tar.gz
+    echo "export GOROOT=/usr/local/go">>~/.profile
+    echo "export GOPATH=$HOME/go-workspace">>~/.profile
+    echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH">>~/.profile
+    source ~/.profile
+    echo "Done"
+    sleep 2
 else
-	echo "Golang already installed!"
+    echo "Golang already installed!"
 fi
 mkdir ~/tools
 cd ~/tools/
@@ -103,68 +114,68 @@ echo "Checking for existing packages"
 
 if [ ! -e ~/go-workspace/bin/aquatone ]
 then
-	#install aquatone
-	echo "Installing Aquatone"
-	go get github.com/michenriksen/aquatone
-	echo "Done"
+    #install aquatone
+    echo "Installing Aquatone"
+    go get github.com/michenriksen/aquatone
+    echo "Done"
 else
-	echo "Aquatone already installed"
+    echo "Aquatone already installed"
 fi
 
 if [ ! -e ~/go-workspace/bin/httprobe ]
 then
-	echo "Installing httprobe"
-	go get -u github.com/tomnomnom/httprobe 
-	echo "Done"
+    echo "Installing httprobe"
+    go get -u github.com/tomnomnom/httprobe
+    echo "Done"
 else
-	echo "httprobe already installed"
+    echo "httprobe already installed"
 fi
 
 if [ ! -e ~/go-workspace/bin/unfurl ]
 then
-	echo "Installing unfurl"
-	go get -u github.com/tomnomnom/unfurl 
-	echo "Done"
+    echo "Installing unfurl"
+    go get -u github.com/tomnomnom/unfurl
+    echo "Done"
 else
-	echo "unfurl already installed"
+    echo "unfurl already installed"
 fi
 
 if [ ! -e ~/go-workspace/bin/waybackurls ]
 then
-	echo "Installing waybackurls"
-	go get github.com/tomnomnom/waybackurls
-	echo "Done"
+    echo "Installing waybackurls"
+    go get github.com/tomnomnom/waybackurls
+    echo "Done"
 else
-	echo "waybackurls already installed"
+    echo "waybackurls already installed"
 fi
 
 if [ ! -e ~/go-workspace/bin/gobuster ]
 then
-	echo "Instaling gobuster"
-	go get github.com/OJ/gobuster
-	echo "Done"
+    echo "Instaling gobuster"
+    go get github.com/OJ/gobuster
+    echo "Done"
 else
-	echo "gobuster already installed"
+    echo "gobuster already installed"
 fi
 
 if [ ! -e ~/go-workspace/bin/ffuf ]
 then
-	echo "Installing ffuf"
-	go get -u github.com/ffuf/ffuf
-	echo "Done"
+    echo "Installing ffuf"
+    go get -u github.com/ffuf/ffuf
+    echo "Done"
 else
-	echo "ffuf already installed"
+    echo "ffuf already installed"
 fi
 
 if [ ! -e ~/go-workspace/bin/nuclie ]
 then
-	echo "Installing nuclie"
-	sudo GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-	echo "Installing nuclie templets"
-	nuclie -update-templates
-	echo "Done"
+    echo "Installing nuclie"
+    sudo GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+    echo "Installing nuclie templets"
+    nuclie -update-templates
+    echo "Done"
 else
-	echo "nuclie already installed"
+    echo "nuclie already installed"
 fi
 
 
@@ -174,171 +185,171 @@ cd ~/tools
 
 if [ ! -d ~/tools/teh_s3_bucketeers ]
 then
-	echo "Installing teh_s3_bucketeers"
-	git clone https://github.com/tomdev/teh_s3_bucketeers.git
-	echo "Done"
+    echo "Installing teh_s3_bucketeers"
+    git clone https://github.com/tomdev/teh_s3_bucketeers.git
+    echo "Done"
 else
-	echo "JSParser already installed"
+    echo "JSParser already installed"
 fi
 
 if [ ! -d ~/tools/JSParser ]
 then
-	echo "Installing JSParser"
-	git clone https://github.com/nahamsec/JSParser.git
-	cd JSParser*
-	sudo python setup.py install
-	cd ~/tools/
-	echo "Done"
+    echo "Installing JSParser"
+    git clone https://github.com/nahamsec/JSParser.git
+    cd JSParser*
+    sudo python setup.py install
+    cd ~/tools/
+    echo "Done"
 else
-	echo "JSParser already installed"
+    echo "JSParser already installed"
 fi
 
 if [ ! -d ~/tools/Sublist3r ]
 then
-	echo "Installing Sublist3r"
-	git clone https://github.com/aboul3la/Sublist3r.git
-	cd Sublist3r*
-	pip install -r requirements.txt
-	python3 setup.py install
-	cd ~/tools/
-	echo "Done"
+    echo "Installing Sublist3r"
+    git clone https://github.com/aboul3la/Sublist3r.git
+    cd Sublist3r*
+    pip install -r requirements.txt
+    python3 setup.py install
+    cd ~/tools/
+    echo "Done"
 else
-	echo "Sublist3r already installed"
+    echo "Sublist3r already installed"
 fi
 
 if [ ! -d ~/tools/dirsearch ]
 then
-	echo "Installing dirsearch"
-	git clone https://github.com/maurosoria/dirsearch.git
-	cd ~/tools/
-	echo "Done"
+    echo "Installing dirsearch"
+    git clone https://github.com/maurosoria/dirsearch.git
+    cd ~/tools/
+    echo "Done"
 else
-	echo "dirsearch already installed"
+    echo "dirsearch already installed"
 fi
 
 if [ ! -d ~/tools/lazys3 ]
 then
-	echo "Installing lazys3"
-	git clone https://github.com/nahamsec/lazys3.git
-	cd ~/tools/
-	echo "Done"
+    echo "Installing lazys3"
+    git clone https://github.com/nahamsec/lazys3.git
+    cd ~/tools/
+    echo "Done"
 else
-	echo "lazys3 already installed"
+    echo "lazys3 already installed"
 fi
 
 if [ ! -d ~/tools/virtual-host-discovery ]
 then
-	echo "Installing virtual host discovery"
-	git clone https://github.com/jobertabma/virtual-host-discovery.git
-	cd ~/tools/
-	echo "Done"
+    echo "Installing virtual host discovery"
+    git clone https://github.com/jobertabma/virtual-host-discovery.git
+    cd ~/tools/
+    echo "Done"
 else
-	echo "virtual-host-discovery already installed"
+    echo "virtual-host-discovery already installed"
 fi
 
 if [ ! -d ~/tools/knock ]
 then
-	echo "Installing knock.py"
-	git clone https://github.com/guelfoweb/knock.git
-	cd ~/tools/
-	echo "Done"
+    echo "Installing knock.py"
+    git clone https://github.com/guelfoweb/knock.git
+    cd ~/tools/
+    echo "Done"
 else
-	echo "knock.py already installed"
+    echo "knock.py already installed"
 fi
 
 if [ ! -d ~/tools/lazyrecon ]
 then
-	echo "Installing lazyrecon"
-	git clone https://github.com/nahamsec/lazyrecon.git
-	cd ~/tools/
-	echo "Done"
+    echo "Installing lazyrecon"
+    git clone https://github.com/nahamsec/lazyrecon.git
+    cd ~/tools/
+    echo "Done"
 else
-	echo "lazyrecon already installed"
+    echo "lazyrecon already installed"
 fi
 
 if [ ! -d ~/tools/massdns ]
 then
-	echo "Installing massdns"
-	git clone https://github.com/blechschmidt/massdns.git
-	cd ~/tools/massdns
-	make
-	make install
-	cd ~/tools/
-	echo "Done"
+    echo "Installing massdns"
+    git clone https://github.com/blechschmidt/massdns.git
+    cd ~/tools/massdns
+    make
+    make install
+    cd ~/tools/
+    echo "Done"
 else
-	echo "massdns already installed"
+    echo "massdns already installed"
 fi
 
 if [ ! -d ~/tools/asnlookup ]
 then
-	echo "Installing asnlookup"
-	git clone https://github.com/yassineaboukir/asnlookup.git
-	cd ~/tools/asnlookup
-	pip install -r requirements.txt
-	cd ~/tools/
-	echo "Done"
+    echo "Installing asnlookup"
+    git clone https://github.com/yassineaboukir/asnlookup.git
+    cd ~/tools/asnlookup
+    pip install -r requirements.txt
+    cd ~/tools/
+    echo "Done"
 else
-	echo "asnlookup already installed"
+    echo "asnlookup already installed"
 fi
 
 if [ ! -d ~/tools/crtndstry ]
 then
-	echo "Installing crtndstry"
-	git clone https://github.com/nahamsec/crtndstry.git
-	echo "Done"
+    echo "Installing crtndstry"
+    git clone https://github.com/nahamsec/crtndstry.git
+    echo "Done"
 else
-	echo "crtndstry already installed"
+    echo "crtndstry already installed"
 fi
 
 if [ ! -d ~/tools/Seclists ]
 then
-	echo "Downloading Seclists"
-	cd ~/tools/
-	git clone https://github.com/danielmiessler/SecLists.git
-	cd ~/tools/SecLists/Discovery/DNS/
-	##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
-	cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-	cd ~/tools/
-	echo "Done"
+    echo "Downloading Seclists"
+    cd ~/tools/
+    git clone https://github.com/danielmiessler/SecLists.git
+    cd ~/tools/SecLists/Discovery/DNS/
+    ##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
+    cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
+    cd ~/tools/
+    echo "Done"
 else
-	echo "Seclists already installed"
+    echo "Seclists already installed"
 fi
 
 if [ ! -d ~/tools/Spiderfoot ]
 then
-	echo "Installing Spiderfoot"
-	cd ~/tools/
-	wget https://github.com/smicallef/spiderfoot/archive/v3.3.tar.gz
-	tar zxvf v3.3.tar.gz
-	cd ~/tools/spiderfoot-3.3
-	pip3 install -r requirements.txt
-	cd ~/tools/
-	rm v3.3.tar.gz
-	echo "Done"
+    echo "Installing Spiderfoot"
+    cd ~/tools/
+    wget https://github.com/smicallef/spiderfoot/archive/v3.3.tar.gz
+    tar zxvf v3.3.tar.gz
+    cd ~/tools/spiderfoot-3.3
+    pip3 install -r requirements.txt
+    cd ~/tools/
+    rm v3.3.tar.gz
+    echo "Done"
 else
-	echo "Spiderfoot already installed"
+    echo "Spiderfoot already installed"
 fi
 
 if [ ! -d ~/tools/enum4linux ]
 then
-	echo "Installing enum4linux"
-	git clone https://github.com/CiscoCXSecurity/enum4linux.git
-	chmod +x enum4linux/enum4linux.pl
-	echo "Done"
+    echo "Installing enum4linux"
+    git clone https://github.com/CiscoCXSecurity/enum4linux.git
+    chmod +x enum4linux/enum4linux.pl
+    echo "Done"
 else
-	echo "enum4linux already installed"
+    echo "enum4linux already installed"
 fi
 
 if [ ! -d ~/opt/dirbuster ]
 then
-	echo "Installing Dirbuster"
-	cd ~/tools/
-	git clone https://gitlab.com/kalilinux/packages/dirbuster.git
-	sudo mv dirbuster /opt
-	alias dirbuster="source /opt/dirbuster/DirBuster-1.0-RC1.sh"
-	echo "Done"
+    echo "Installing Dirbuster"
+    cd ~/tools/
+    git clone https://gitlab.com/kalilinux/packages/dirbuster.git
+    sudo mv dirbuster /opt
+    alias dirbuster="source /opt/dirbuster/DirBuster-1.0-RC1.sh"
+    echo "Done"
 else
-	echo "Dirbuster already installed"
+    echo "Dirbuster already installed"
 fi
 
 echo "Installing Metasploit-framework"
@@ -347,8 +358,8 @@ rm msfinstall
 echo "Done"
 
 echo "Installing Maletgo"
-curl -LO https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.2.15.13632.deb
-sudo dpkg -i Maltego.v4.2.15.13632.deb
+curl -LO https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.3.1.deb
+sudo dpkg -i Maltego.v4.3.1.deb
 sleep 1
 echo "Done"
 
@@ -367,7 +378,7 @@ clear
 
 echo -e "$red"
 figlet "Offensive Ubuntu"
-echo "Github : https://github.com/Ryuk0x01"
+echo "Github : https://github.com/0xRyuk"
 echo -e "$reset"
 echo "Installation compeleted."
 echo "Your ubuntu is ready to use!"
